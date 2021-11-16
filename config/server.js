35b2122eb628bd2982acd6db43f50fa452ee18de
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-11-16 11:30:43 -0600

    Initial Commit

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '244227fcbb6f81ebb86f35349381b824'),
+      secret: env("ADMIN_JWT_SECRET", "244227fcbb6f81ebb86f35349381b824"),
     },
   },
 });
